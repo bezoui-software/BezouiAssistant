8cd32b97ed2cb9d1dcc2f813b4d89e5a5e60ad4d
bezoui-software <64211294+bezoui-software@users.noreply.github.com>
2021-02-22 22:10:19 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -260,7 +260,18 @@
       setup();
     }
 
+    function setupServiceWorker() {
+      if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('../sw.js').then(function(registration) {
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+          console.log('ServiceWorker registration failed: ', err);
+        });
+      }
+    }
+
     function setup() {
+      setupServiceWorker();
       document.querySelector('#send-question-button').addEventListener('click', evaluateUserQuestion);
       document.querySelector('#send-voice-question-button').addEventListener('click', evaluateUserVoiceQuestion);
       window.addEventListener('keypress', e => {
