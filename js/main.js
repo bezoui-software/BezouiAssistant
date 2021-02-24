f3853fadaf8183de1d0cab9d4a84e1945ceaf115
bezoui-software <64211294+bezoui-software@users.noreply.github.com>
2021-02-24 09:18:52 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -285,7 +285,7 @@
 
     function setupServiceWorker() {
       if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('../sw.js').then(function(registration) {
+        navigator.serviceWorker.register('BezouiAssistant/sw.js').then(function(registration) {
           console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function(err) {
           console.log('ServiceWorker registration failed: ', err);
